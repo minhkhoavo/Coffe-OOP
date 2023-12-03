701a2d58d21dffa11bf833e58efb20dfa798fda8
minhkhoavo <v.minhkhoa123456@gmail.com>
2023-12-03 11:00:08 +0700

    ui: login form

--- OOP-Coffee/Form/fLogin.Designer.cs
+++ OOP-Coffee/Form/fLogin.Designer.cs
@@ -29,135 +29,190 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.customerRadio = new System.Windows.Forms.RadioButton();
-            this.baristaRadio = new System.Windows.Forms.RadioButton();
             this.managerRadio = new System.Windows.Forms.RadioButton();
+            this.baristaRadio = new System.Windows.Forms.RadioButton();
+            this.customerRadio = new System.Windows.Forms.RadioButton();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
             this.groupBox1.Controls.Add(this.managerRadio);
             this.groupBox1.Controls.Add(this.baristaRadio);
             this.groupBox1.Controls.Add(this.customerRadio);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.groupBox1.Location = new System.Drawing.Point(27, 32);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(740, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(532, 96);
+            this.groupBox1.Size = new System.Drawing.Size(437, 95);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Login as";
             // 
-            // customerRadio
-            // 
-            this.customerRadio.AutoSize = true;
-            this.customerRadio.Location = new System.Drawing.Point(49, 39);
-            this.customerRadio.Name = "customerRadio";
-            this.customerRadio.Size = new System.Drawing.Size(147, 28);
-            this.customerRadio.TabIndex = 0;
-            this.customerRadio.TabStop = true;
-            this.customerRadio.Text = "Khách hàng";
-            this.customerRadio.UseVisualStyleBackColor = true;
-            // 
-            // baristaRadio
-            // 
-            this.baristaRadio.AutoSize = true;
-            this.baristaRadio.Location = new System.Drawing.Point(238, 39);
-            this.baristaRadio.Name = "baristaRadio";
-            this.baristaRadio.Size = new System.Drawing.Size(97, 28);
-            this.baristaRadio.TabIndex = 0;
-            this.baristaRadio.TabStop = true;
-            this.baristaRadio.Text = "Barista";
-            this.baristaRadio.UseVisualStyleBackColor = true;
-            // 
             // managerRadio
             // 
             this.managerRadio.AutoSize = true;
-            this.managerRadio.Location = new System.Drawing.Point(384, 39);
+            this.managerRadio.Location = new System.Drawing.Point(325, 38);
             this.managerRadio.Name = "managerRadio";
-            this.managerRadio.Size = new System.Drawing.Size(107, 28);
+            this.managerRadio.Size = new System.Drawing.Size(105, 29);
             this.managerRadio.TabIndex = 0;
             this.managerRadio.TabStop = true;
             this.managerRadio.Text = "Quản lý";
             this.managerRadio.UseVisualStyleBackColor = true;
             // 
+            // baristaRadio
+            // 
+            this.baristaRadio.AutoSize = true;
+            this.baristaRadio.Location = new System.Drawing.Point(209, 38);
+            this.baristaRadio.Name = "baristaRadio";
+            this.baristaRadio.Size = new System.Drawing.Size(97, 29);
+            this.baristaRadio.TabIndex = 0;
+            this.baristaRadio.TabStop = true;
+            this.baristaRadio.Text = "Barista";
+            this.baristaRadio.UseVisualStyleBackColor = true;
+            // 
+            // customerRadio
+            // 
+            this.customerRadio.AutoSize = true;
+            this.customerRadio.Location = new System.Drawing.Point(50, 38);
+            this.customerRadio.Name = "customerRadio";
+            this.customerRadio.Size = new System.Drawing.Size(140, 29);
+            this.customerRadio.TabIndex = 0;
+            this.customerRadio.TabStop = true;
+            this.customerRadio.Text = "Khách hàng";
+            this.customerRadio.UseVisualStyleBackColor = true;
+            // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(29, 134);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(806, 129);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(530, 141);
+            this.panel1.Size = new System.Drawing.Size(322, 193);
             this.panel1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.label1.Location = new System.Drawing.Point(30, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 28);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Tên đăng nhập";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.label2.Location = new System.Drawing.Point(30, 91);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 28);
-            this.label2.TabIndex = 0;
-            this.label2.Text = "Mật khẩu";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(198, 33);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(296, 26);
-            this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(198, 95);
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.textBox2.Location = new System.Drawing.Point(56, 21);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(296, 26);
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(226, 32);
             this.textBox2.TabIndex = 1;
+            this.textBox2.UseWaitCursor = true;
             // 
-            // button1
+            // textBox1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.button1.Location = new System.Drawing.Point(134, 303);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(304, 54);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Đăng nhập";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.textBox1.Location = new System.Drawing.Point(57, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(203, 24);
+            this.textBox1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Location = new System.Drawing.Point(3, 101);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(311, 67);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Location = new System.Drawing.Point(3, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(313, 66);
+            this.panel3.TabIndex = 3;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::OOP_Coffee.Properties.Resources.Group_3;
+            this.pictureBox4.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(716, 472);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::OOP_Coffee.Properties.Resources.Button;
+            this.pictureBox3.Location = new System.Drawing.Point(806, 351);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(322, 67);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::OOP_Coffee.Properties.Resources.Email;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(307, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::OOP_Coffee.Properties.Resources.Password;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(308, 60);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // fLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(587, 369);
-            this.Controls.Add(this.button1);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1189, 468);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "fLogin";
             this.Text = "fLogin";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -170,9 +225,12 @@
         private System.Windows.Forms.RadioButton customerRadio;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
--- OOP-Coffee/Form/fLogin.cs
+++ OOP-Coffee/Form/fLogin.cs
@@ -18,6 +18,7 @@ namespace OOP_CoffeeApp
         public fLogin()
         {
             InitializeComponent();
+            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -80,5 +81,79 @@ namespace OOP_CoffeeApp
             }
             
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.BringToFront();
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Focus();
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.BringToFront();
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Focus();
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string username = textBox1.Text;
+                string password = textBox2.Text;
+                if (customerRadio.Checked)
+                {
+                    if (Customer.checkLogin(username, password))
+                    {
+                        MessageBox.Show("Success login");
+                        Form1 customerForm = new Form1(int.Parse(username));
+                        customerForm.Show();
+                        this.DialogResult = DialogResult.OK;
+                        //this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Vui lòng thử lại. Sai username hoặc Password");
+                    }
+                }
+                else if (baristaRadio.Checked)
+                {
+                    if (Barista.checkLogin(username, password))
+                    {
+                        MessageBox.Show("Success login");
+
+                        ParistaFrm baristaForm = new ParistaFrm();
+                        baristaForm.Show();
+                        this.DialogResult = DialogResult.OK;
+                        //this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Vui lòng thử lại. Sai username hoặc Password");
+                    }
+                }
+                else
+                {
+                    if (Manager.checkLogin(username, password))
+                    {
+                        MessageBox.Show("Success login");
+
+                        fAdmin fAminForm = new fAdmin();
+                        fAminForm.Show();
+                        this.DialogResult = DialogResult.OK;
+                        //this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Vui lòng thử lại. Sai username hoặc Password");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
     }
 }
